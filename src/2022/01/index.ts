1910c3e3d29a9d85df1528a36bd38534c87f518f
iamkaarp <joakim.stenberg88@gmail.com>
2022-12-01 09:24:08 +0100

    2022/01 result

---
+++ src/2022/01/index.ts
@@ -0,0 +1,49 @@
+async function taskA(input: string): Promise<number> {
+  const elves: Array<string> = input.split("\n\n")
+
+  const caloryCount: Array<number> = elves.reduce(
+    (result: Array<number>, elf: string) => {
+      const caloriesPerElf: Array<string> = elf.split("\n")
+      result.push(
+        caloriesPerElf.reduce((total, calories: string) => {
+          total += Number(calories)
+          return total
+        }, 0)
+      )
+
+      return result
+    },
+    new Array<number>()
+  )
+  const result = caloryCount.sort((a: number, b: number) => b - a)[0]
+
+  return result
+}
+
+async function taskB(input: string): Promise<number> {
+  const elves: Array<string> = input.split("\n\n")
+
+  const caloryCount: Array<number> = elves.reduce(
+    (result: Array<number>, elf: string) => {
+      const caloriesPerElf: Array<string> = elf.split("\n")
+      result.push(
+        caloriesPerElf.reduce((total, calories: string) => {
+          total += Number(calories)
+          return total
+        }, 0)
+      )
+
+      return result
+    },
+    new Array<number>()
+  )
+  const sorted = caloryCount.sort((a: number, b: number) => b - a).slice(0, 3)
+  const result = sorted.reduce((total: number, calories: number) => {
+    total += calories
+    return total
+  }, 0)
+
+  return result
+}
+
+export { taskA, taskB }
