bf2d396bacd0f0bda3801c2163b370114207c0bf
iamkaarp <joakim.stenberg88@gmail.com>
2022-12-01 09:42:00 +0100

    refactoring, spelling

--- src/2022/01/index.ts
+++ src/2022/01/index.ts
@@ -1,7 +1,7 @@
 async function taskA(input: string): Promise<number> {
   const elves: Array<string> = input.split("\n\n")
 
-  const caloryCount: Array<number> = elves.reduce(
+  const calorieCount: Array<number> = elves.reduce(
     (result: Array<number>, elf: string) => {
       const caloriesPerElf: Array<string> = elf.split("\n")
       result.push(
@@ -15,15 +15,14 @@ async function taskA(input: string): Promise<number> {
     },
     new Array<number>()
   )
-  const result = caloryCount.sort((a: number, b: number) => b - a)[0]
 
-  return result
+  return calorieCount.sort((a: number, b: number) => b - a)[0]
 }
 
 async function taskB(input: string): Promise<number> {
   const elves: Array<string> = input.split("\n\n")
 
-  const caloryCount: Array<number> = elves.reduce(
+  const calorieCount: Array<number> = elves.reduce(
     (result: Array<number>, elf: string) => {
       const caloriesPerElf: Array<string> = elf.split("\n")
       result.push(
@@ -37,15 +36,14 @@ async function taskB(input: string): Promise<number> {
     },
     new Array<number>()
   )
-  const result = caloryCount
+
+  return calorieCount
     .sort((a: number, b: number) => b - a)
     .slice(0, 3)
     .reduce((total: number, calories: number) => {
       total += calories
       return total
     }, 0)
-
-  return result
 }
 
 export { taskA, taskB }
